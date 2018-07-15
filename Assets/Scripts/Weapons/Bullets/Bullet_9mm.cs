3432e77e41835eb6810ba8920d678ad6eeb79a6d
kirakishou <kez1911@protonmail.com>
2018-07-15 15:56:25 +0300

    Add P250

---
+++ Assets/Scripts/Weapons/Bullets/Bullet_9mm.cs
@@ -0,0 +1,32 @@
+﻿namespace Weapons.Bullets{
+
+	public class Bullet_9mm : BaseBullet{
+		private float speedDrag = 60f;
+		private float stoppingFactor = 100f;
+		private float penetrationFactor = .2f;
+
+		public const float baseSpeed = 110f;
+		public const int baseDamage = 8;
+
+		public override float getBaseSpeed() {
+			return baseSpeed;
+		}
+
+		public override int getBaseDamage() {
+			return baseDamage;
+		}
+
+		public override float getStoppingFactor() {
+			return stoppingFactor;
+		}
+
+		public override float getSpeedDrag() {
+			return speedDrag;
+		}
+
+		public override float getPenetrationFactor() {
+			return penetrationFactor;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Weapons/P250.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+using Weapons.Bullets;
+
+namespace Weapons{
+
+	public class P250 : BaseWeapon{
+		private const float minSpread = .005f;
+		private const float maxSpread = .05f;
+		private float weaponSpread = minSpread;
+		
+		public Bullet_9mm bullet;
+		public Transform firePoint;
+		private float timeBetweenShots = .1f;
+
+		private void Update() {
+			updateSpread();
+			base.Update();
+		}
+
+		public override void startShooting() {
+			if (!isFiring) {
+				StartCoroutine(shoot());
+			}
+		}
+
+		public override void stopShooting() {
+		}
+		
+		private void updateSpread() {
+			weaponSpread -= (minSpread * 3) * Time.deltaTime;
+			if (weaponSpread < minSpread) {
+				weaponSpread = minSpread;
+			}
+		}
+
+		private IEnumerator shoot() {
+			isFiring = true;
+			spawnBullet();
+			yield return new WaitForSeconds(timeBetweenShots);
+			isFiring = false;
+		}
+
+		private void spawnBullet() {
+			weaponSpread += minSpread;
+
+			if (weaponSpread > maxSpread) {
+				weaponSpread = maxSpread;
+			}
+			
+			var newWeaponSpread = weaponSpread;
+			var playerSpeed = player.getPlayerSpeed();
+		
+			if (playerSpeed <= .1f) {
+				newWeaponSpread /= 5f;
+			}
+		
+			if (playerSpeed > .1f && playerSpeed <= player.getPlayerAimingSpeed()) {
+				newWeaponSpread /= 3f;
+			}
+			
+			var rotation = firePoint.rotation;
+			rotation.y += Random.Range(-newWeaponSpread, newWeaponSpread);
+
+			Instantiate(bullet, firePoint.position, rotation);
+		}
+
+		public override WeaponShootType getWeaponShootType() {
+			return WeaponShootType.SemiAutomatic;
+		}
+
+		public override float getWeaponSpread() {
+			return weaponSpread;
+		}
+	}
+
+}
